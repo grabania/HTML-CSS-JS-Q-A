beb59b467d3d31b6d20d326c350aa5b2e7bc17e7
grabania <grabania.work@gmail.com>
2021-02-03 23:13:58 +0000

    Update

---
+++ script.js
@@ -0,0 +1,47 @@
+/***   question1  ***/
+
+// function validateSSN(userinput) {
+//   var ssnPattern = /^[0-9]{3}\^?[0-9]{2}\^?[0-9]{4}$/
+//   return ssnPattern.test(userinput);
+// }
+
+// function validateEmail(userinput) {
+//   var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+//   return emailPattern.test(userinput)
+// }
+
+/***   
+ * 
+ * question2  
+ * 
+ 
+You are developing a customer web form that includes the following HTML.
+<input id="txtValue" type="text" />
+A customer must enter a valid age in the text box prior to submitting the form.
+You need to add validation to the control.
+
+***/
+
+// function validate() {
+//   var value = $("#txtValue").val()
+//   var regex = /^[\d\,\.]*$/
+//   if (!regex.test(value) || value == "") alert("please enter valid value")
+//   return
+// }
+
+// return emailPatter.test(userinput)
+
+/***   
+ * 
+ * question3  
+ * 
+ 
+You are developing a customer contact form that will be displayed on a page of a company's website.
+The page collects information about the customer.
+If a customer enters a value before submitting the form, it must be a valid email address.
+You need to ensure that the data validation requirement is met.
+What should you use?
+
+***/
+
+// <input name='email' type='email' />
